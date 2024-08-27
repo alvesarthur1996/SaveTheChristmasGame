4d715b5550812607e56b89237d1b887e6ef819d4
Arthur Santos <arthur_alves.santos@outlook.com.br>
2024-08-27 18:47:25 -0300

    refactor: add new global functions and refactor some

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -3,6 +3,7 @@ import CandyBoomerang from "../controllers/weapons/candyBoomerang";
 import IceBlock from "../controllers/weapons/iceBlock";
 import LaserBeam from "../controllers/weapons/laserBeam";
 import SnowBuster from "../controllers/weapons/snowBuster"
+import DefaultScene from "../scenes/defaultScene";
 import { BossWeapon } from "./boss";
 import { SoundOptions } from "./options";
 import { Weapons, WeaponsAtlas } from "./weapons"
@@ -16,7 +17,7 @@ export type BulletShootConfig = {
 }
 
 
-export function callWeaponClassDinamically(classname: Weapons|BossWeapon, data: BulletShootConfig): BulletShoot | undefined {
+export function callWeaponClassDinamically(classname: Weapons | BossWeapon, data: BulletShootConfig): BulletShoot | undefined {
     switch (classname) {
         case Weapons.SnowBuster:
             return new SnowBuster(data.world, data.x, data.y, WeaponsAtlas.SnowBuster, data.bodyOptions, data.soundOptions);
@@ -26,5 +27,17 @@ export function callWeaponClassDinamically(classname: Weapons|BossWeapon, data: 
             return new LaserBeam(data.world, data.x, data.y, WeaponsAtlas.LaserBeam, data.bodyOptions, data.soundOptions);
         case Weapons.IceBlock:
             return new IceBlock(data.world, data.x, data.y, WeaponsAtlas.IceBlock, data.bodyOptions, data.soundOptions);
+    }
+}
+
+
+export function createParallaxImage(scene: DefaultScene, count: number, image: object, scrollFactor: number, offsetY: number = 0, offsetX: number = 0) {
+    let x = image.x + offsetX;
+    console.log(x)
+    for (let i = 1; i <= count; i++) {
+        const img = scene.add.image(x, image.y + offsetY, image.name)
+            .setOrigin(0, 0)
+            .setScrollFactor(scrollFactor);
+        x += img.width;
     }
 }

2b03b114d9429828129ba06ef38ab10da208abdf
Arthur Santos <arthur_alves.santos@outlook.com.br>
2025-05-09 09:08:13 -0300

    server: add new server providing endpoints

--- server/server.js
+++ server/server.js
@@ -13,11 +13,33 @@ app.use(cors())
 app.use(helmet())
 
 app.use('/static', express.static(path.join(__dirname, '../src/assets')))
+
+app.use('/game-state', (req, res) => {
+  const filePath = path.join(__dirname, '../game_state.json');
+  const response = JSON.parse(fs.readFileSync(filePath));
+
+  return res.status(200).send(response);
+});
+
+app.use('/update-game-state', (req, res) => {
+  try {
+    const filePath = path.join(__dirname, '../game_state.json');
+    fs.writeFileSync(filePath, JSON.stringify(req.body));
+    console.log('updated');
+  } catch (e) {
+    console.log(e);
+  }
+  
+  return res.status(200).send({ message: 'Game state updated' });
+});
+
 app.use('/load-options', (req, res) => {
   const filePath = path.join(__dirname, '../game_settings.json');
   const response = JSON.parse(fs.readFileSync(filePath));
+
   return res.status(200).send(response);
 });
+
 app.use('/save-options', (req, res) => {
 
   try {
